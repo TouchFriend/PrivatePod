f9a81f7dec145319a874be6c2f340c0c79e559a0
cxz <1604101058@qq.com>
2019-04-21 14:39:04 +0800

    0.0.2

--- Example/PrivatePod/RJViewController.m
+++ Example/PrivatePod/RJViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     RJPerson *person = [[RJPerson alloc] init];
-    [person test2];
+    [person test3];
 }
 
 - (void)didReceiveMemoryWarning
